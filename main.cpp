e9c138ac2ffc9ac1fee8d68790b02ba3c7f4b817
BAgithub2022 <117846641+BAgithub2022@users.noreply.github.com>
2022-11-12 09:35:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+float a=0,b=0,c=0,triangolo,quadrato,rettangolo,trapezio;
 
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
+triangolo=a*b/2,quadrato=a*a,rettangolo=a*b,trapezio=(a+b)*c/2;
+ cout << triangolo << endl;
+ cout << quadrato << endl;
+ cout << rettangolo << endl;
+ cout << trapezio << endl;
 
 }
